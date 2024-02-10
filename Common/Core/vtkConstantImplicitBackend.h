dfb082572430cf4bb2c0718e516c07cbeb997283
Stefan Dinkelacker <s.dinkelacker@dkfz-heidelberg.de>
2024-02-10 23:46:34 +0100

    Fix debug build with Visual Studio

--- Common/Core/vtkConstantImplicitBackend.h
+++ Common/Core/vtkConstantImplicitBackend.h
@@ -32,7 +32,7 @@
  */
 VTK_ABI_NAMESPACE_BEGIN
 template <typename ValueType>
-struct VTKCOMMONCORE_EXPORT vtkConstantImplicitBackend final
+struct vtkConstantImplicitBackend final
 {
   /**
    * A non-trivially contructible constructor
